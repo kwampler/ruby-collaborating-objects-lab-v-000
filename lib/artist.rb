fc64b7efbefa50873d157a5235ec37e95039f57f
Kate Preston <katejsenger@yahoo.com>
2018-11-20 03:07:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -40,7 +40,7 @@ class Artist
     artist
   end
 
-  def print_songs #instance method iterates through @songs of an instance of Artist
-    self.songs.each {|song| song.name}
+  def print_songs()
+    puts @songs.collect {|x| x.name}
   end
 end
